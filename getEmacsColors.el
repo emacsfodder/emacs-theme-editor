11aba391012271ac0b5dae28d6cbaaff6037b8c2
Jasonm23 <jasonm23@gmail.com>
2015-03-14 17:04:59 +0800

    Converting to Bootstrap 3 and using Spectrum color picker, lots more.

--- getEmacsColors.el
+++ getEmacsColors.el
@@ -1,41 +1,21 @@
+;; Quickly grab all the named colors from Emacs
+;; this is intended to be run with `M-x load-file`
+(defun color-hex ( i )
+  (format "%02X" (floor (* 255 (/ (float i) (float 65535) )))))
+
 (progn
-  
-  ;; The number of defined-colors ... probably won't need this... :)
-  ;; (setq c (list-length (defined-colors)))
-
   (insert "var emacsColors = [\n")
-
   (setq beg (point))
-
   (loop for c in (defined-colors)
-	if (numberp (nth 0 (color-values c)))
-	do 
-	(insert "    { name: \"" c "\", ")
-	;; Dirty align coloumn... 
-	;; (insert (make-string (- 50 (current-column)) 32))
-	;; Better to note the length of defined-colors and align-regexp
-        ;; on a computed region. 
-        ;; (will use values from a & c to set region)
-	(insert "color: \"#" 
-		(color-hex
-		 (nth 0 (color-values c))) 
-		(color-hex
-		 (nth 1 (color-values c))) 
-		(color-hex
-		 (nth 2 (color-values c))) 
-		)
-	(insert "\" },\n")
-	)
+        if (numberp (nth 0 (color-values c)))
+        do
+        (insert "    { name: \"" c "\", ")
+        (insert "color: \"#"
+                (color-hex (nth 0 (color-values c)))
+                (color-hex (nth 1 (color-values c)))
+                (color-hex (nth 2 (color-values c))))
+        (insert "\" },\n"))
 
   (align-regexp beg (point) "color:")
-
   (backward-delete-char 2 )
-
-  (insert "\n];")
-  ;; need to add - set region from row a to row b.
-
-  (defun color-hex ( i )
-    (format "%02X" (floor (* 255 (/ (float i) (float 65535) ))))
-    )
-  )
-
+  (insert "\n];"))
